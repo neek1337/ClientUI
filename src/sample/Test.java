06f931d296766c97d3d8745a8ab5399eff1f20ef
tkmaqi3 <fedor.paramonov@kohls.com>
2016-12-02 18:58:23 +0300

    Gost & Test & Clean

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -9,11 +9,6 @@ import java.nio.file.Paths;
  */
 public class Test {
     public static void main(String[] args) throws IOException {
-        byte[] keyB = Files.readAllBytes(Paths.get("smallKey.txt"));
-        String keyS = new String(keyB);
-        BBs generator = new BBs(keyS);
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(generator.next());
-        }
+
     }
 }
